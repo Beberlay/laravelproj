b7b8341fe56244499c5b9f6b9375b4edee962679
Beberlay <beberllypalconit76@gmail.com>
2023-10-03 15:51:25 +0800

    added activity # 5

--- database/migrations/2023_09_29_065214_create_courses_and_instructors_tables.php
+++ database/migrations/2023_09_29_065214_create_courses_and_instructors_tables.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('courses_and_instructors_tables', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('description')->nullable;
